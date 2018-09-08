b50a67fe1d17f8800da51d0b07687b9a20a8f206
imwaykul <imwaykul@gmail.com>
2018-09-08 02:44:59 -0400

    Add files via upload

--- ImageEncoding.py
+++ ImageEncoding.py
@@ -40,7 +40,11 @@ for img in range(1, 872):
         pass
 
 print("SMALLEST RES: ", smallestRes)
+print("LENGTH: ", length)
+print("WIDTH: ", width)
 print("IMG ID: ", imgId)
+length = 224
+width = 224
 
 for img in range(1, 872):
     indiv_img_array = []
@@ -60,8 +64,10 @@ for img in range(1, 872):
             img_info = "image_0" + str(img)+ ".pts"
         try:
             im = Image.open(full_img_str)
+            cv_img = cv2.imread(full_img_str)
             pic_length_ratio= length/im.size[0]
             pic_width_ratio = width/im.size[1]
+            img_smaller = cv2.resize(cv_img,(0,0),fx=pic_length_ratio,fy=pic_width_ratio)
             open_pts_file = open(img_info)
             ctr = 0
             for line in open_pts_file.readlines():
@@ -70,10 +76,37 @@ for img in range(1, 872):
                     x = int(float(coord[0]) * pic_length_ratio)
                     y = int(float(coord[1]) * pic_width_ratio)
                     indiv_img_array.append((x,y))
+                    cv2.circle(img_smaller,(x,y), 3, (0,255,0), -1)
+                    cv2.imshow("face1Small",img_smaller)
+                    cv2.waitKey(100)
                 ctr = ctr + 1
                 newstr = "RESCALED" + full_img_str
             image_array.append(indiv_img_array)
         except:
             pass
-        
-print(len(image_array))
+#print(len(image_array))
+
+            
+
+
+##img = cv2.imread('image_0001.png')
+##
+##scale = 1.5
+##
+##imgSmaller = cv2.resize(img,(0,0),fx=scale,fy=scale)
+##cv2.circle(imgSmaller,(int(115*scale),int(229*scale)), 3, (0,255,0), -1)
+##cv2.circle(imgSmaller,(int(116*scale),int(245*scale)), 3, (0,255,0), -1)
+##cv2.circle(imgSmaller,(int(120*scale),int(270*scale)), 3, (0,255,0), -1)
+##cv2.imshow("face1Small",imgSmaller)
+##
+##
+##cv2.circle(img,(115,229), 3, (255,0,0), -1)
+##cv2.circle(img,(116,245), 3, (255,0,0), -1)
+##cv2.circle(img,(120,270), 3, (255,0,0), -1)
+##
+##cv2.imshow("face1",img)
+##
+##print (img.shape)
+##print (imgSmaller.shape)
+##cv2.waitKey(0)
+
